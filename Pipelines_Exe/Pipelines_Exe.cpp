869ee7ada2bee7fa64598967dac9e80273a157e6
Saketh Jaggaiahgari <jaggaish@mail.uc.edu>
2024-04-12 01:20:48 -0400

    Pipelines_exe has been added

---
+++ Pipelines_Exe/Pipelines_Exe.cpp
@@ -0,0 +1,23 @@
+// Pipelines_Exe.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "../CI_CD_Assgmnt_Grp_35/CI_CD_Assgmnt_Grp_35.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    CCICDAssgmntGrp35 c1;
+    std::cout << "Calc" << c1.calc(1, 2);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
